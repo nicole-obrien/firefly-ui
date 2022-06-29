5ea2a7cfaff2af7d942a2fcaa637cf28e45c6c07
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-29 10:09:40 +0100

    fix: missing configuration files

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-svelte-csf',
+        '@storybook/addon-storysource',
+        '@storybook/addon-a11y',
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss')
+                }
+            }
+        }
+    ],
+    framework: '@storybook/svelte',
+    svelteOptions: {
+        preprocess: require('svelte-preprocess')({postcss: true})
+    }
+};
