e65b40af807fdba55c519bab065cea8a506cd3ea
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-30 09:57:18 +0100

    feat: add pill component

---
+++ src/components/pills/Pill.stories.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Pill from './Pill.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta title="Example/Pills/Pill" component={Pill} />
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <div class="flex items-center justify-center">
+        <Pill {...args} />
+    </div>
+</Template>
+
+<Story
+    name="Default"
+    args={{
+        data: 'data'
+    }}
+/>
---
+++ src/components/pills/Pill.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import Text, {FontWeightText} from '../text/Text.svelte';
+
+    export let data = '';
+    export let textColor = 'gray-800';
+    export let darkTextColor = 'gray-800';
+    export let backgroundColor = 'gray-300';
+    export let darkBackgroundColor = 'gray-300';
+    export let classes = '';
+    export let textClasses = '';
+</script>
+
+<pill
+    class={`
+        flex-none
+        px-2.5 py-1 rounded-2xl
+        ${backgroundColor ? 'bg-' + backgroundColor : ''}
+        ${darkBackgroundColor ? 'dark:bg-' + darkBackgroundColor : ''}
+        ${classes}
+    `}
+>
+    <Text
+        smaller
+        fontWeight={FontWeightText.semibold}
+        color={textColor}
+        darkColor={darkTextColor || textColor}
+        classes={`
+
+            ${textClasses}
+        `}
+    >
+        {#if data}
+            {data}
+        {:else}
+            <slot />
+        {/if}
+    </Text>
+</pill>
